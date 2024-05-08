dc5fa271a2f6cac31675ff92417ea874f30afb57
Johannes Graner <johannes.graner@silo.ai>
2024-05-08 14:55:15 +0200

    Activate plugins and add colorschema and leap

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
@@ -8,6 +6,36 @@ return {
 
   -- == Examples of Adding Plugins ==
 
+  "catppuccin/nvim",
+  {
+    "ggandor/leap.nvim",
+    lazy = false,
+    config = function()
+      local leap = require "leap"
+      leap.add_default_mappings(true)
+      leap.opts.highlight_unlabeled_phase_one_targets = true
+      leap.setup {
+        special_keys = {
+          repeat_search = "<enter>",
+          next_phase_one_target = "<enter>",
+          next_target = { "<enter>", ";" },
+          prev_target = { "<tab>", "," },
+          next_group = "<tab>",
+          prev_group = "<s-tab>",
+          multi_accept = "<enter>",
+          multi_revert = "<backspace>",
+        },
+      }
+    end,
+  },
+  {
+    "ggandor/flit.nvim",
+    lazy = false,
+    dependencies = { "ggandor/leap.nvim" },
+    config = function() require("flit").setup() end,
+  },
+
+  -- Added by AstroNvim template
   "andweeb/presence.nvim",
   {
     "ray-x/lsp_signature.nvim",
