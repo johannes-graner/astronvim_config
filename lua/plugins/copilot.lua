247570371b5892d09d0c0573e071626311a4b544
Johannes Graner <johannes.graner@amd.com>
2025-03-24 09:06:01 +0100

    Copilot keymaps

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -18,6 +18,48 @@ return {
     },
   },
   {
+    "AstroNvim/astrocore",
+    opts = function(_, opts)
+      local nmaps = opts.mappings.n
+      local vmaps = opts.mappings.v
+
+      local toggle = {
+        key = "<Leader>a",
+        value = {
+          "<cmd>CodeCompanionChat Toggle<cr>",
+          desc = "Toggle AI Chat",
+          noremap = true,
+          silent = true,
+        },
+      }
+      nmaps[toggle.key] = toggle.value
+      vmaps[toggle.key] = toggle.value
+
+      local actions = {
+        key = "C-a",
+        value = {
+          "<cmd>CodeCompanionActions<cr>",
+          noremap = true,
+          silent = true,
+        },
+      }
+      nmaps[actions.key] = actions.value
+      vmaps[actions.key] = actions.value
+
+      local prompt = {
+        key = "<Leader>P",
+        value = {
+          "<cmd>CodeCompanion<cr>",
+          desc = "Prompt LLM",
+          noremap = true,
+          silent = true,
+        },
+      }
+      nmaps[prompt.key] = prompt.value
+      vmaps[prompt.key] = prompt.value
+    end,
+  },
+  {
     "github/copilot.vim",
     config = function()
       vim.api.nvim_set_keymap("i", "<S-CR>", 'copilot#Accept("<CR>")', { expr = true, noremap = true, silent = true })
