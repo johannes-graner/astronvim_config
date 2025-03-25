c2be8642526e98d9fa0565c6095a827557db0cc4
Johannes Graner <johannes.graner@amd.com>
2025-03-25 11:39:19 +0100

    Fix live grep with args and reference search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,15 +3,39 @@ return {
   {
     "nvim-telescope/telescope-live-grep-args.nvim",
     dependencies = { "nvim-telescope/telescope.nvim" },
-    config = function() require("telescope").load_extension "live_grep_args" end,
+    config = function()
+      local telescope = require "telescope"
+      local lga_actions = require "telescope-live-grep-args.actions"
+      telescope.setup {
+        extensions = {
+          live_grep_args = {
+            auto_quoting = true,
+            mappings = {
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+                ["<C-i>"] = lga_actions.quote_prompt { postfix = " --iglob " },
+                ["<C-space>"] = lga_actions.to_fuzzy_refine,
+              },
+            },
+          },
+        },
+      }
+      telescope.load_extension "live_grep_args"
+    end,
   },
   {
     "AstroNvim/astrocore",
-    opts = function(_, opts)
-      opts.mappings.n["<leader>fw"] = {
-        function() require("telescope").extensions.live_grep_args.live_grep_args() end,
-        desc = "Live Grep with arguments",
-      }
-    end,
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>fw"] = {
+            function() require("telescope").extensions.live_grep_args.live_grep_args() end,
+            desc = "Live Grep with arguments",
+          },
+          grr = { require("telescope.builtin").lsp_references },
+        },
+      },
+    },
   },
 }
